b64563a6a88dcd8f848febf1c6566506d18ef5c3
Caroll Molessani <carollmolessani@gmail.com>
2021-07-19 12:58:51 -0300

    Verificação de ciclos negativos

--- src/main/java/min_paths_algos/Bellman_Ford.java
+++ src/main/java/min_paths_algos/Bellman_Ford.java
@@ -7,7 +7,7 @@ import graph.linked_list.Node;
 
 public class Bellman_Ford {
 
-    public static void BF( Digraph dg, int sourceVertexValue) throws NonSourceVertexException{
+    public static boolean BF(Digraph dg, int sourceVertexValue) throws NonSourceVertexException {
 
 //        dg.getWeightBetween();
         //initialize single source
@@ -16,13 +16,13 @@ public class Bellman_Ford {
         AdjacentList[] arrayOfAdjacentList = dg.getArrayOfAdjacentLists();
 
         //o algoritmo faz |V| – 1 passagens pelas arestas do grafo
-        for(int i=0; i < arrayOfAdjacentList.length ;i++){
+        for (int i = 0; i < arrayOfAdjacentList.length; i++) {
 
             //se a posição estiver vazia, próximo
             if (arrayOfAdjacentList[i] == null) continue;
 
             //iteração em cada lista de adjacencia
-            for(int j=0; j < arrayOfAdjacentList.length;j++){
+            for (int j = 0; j < arrayOfAdjacentList.length; j++) {
 
                 //se a posição estiver vazia, próximo
                 if (arrayOfAdjacentList[j] == null) continue;
@@ -31,15 +31,32 @@ public class Bellman_Ford {
                 Node sourceNode = temp.getHeadNode();
                 Node endNode = sourceNode.getNextNode();
 
-                while (endNode != null){
-                     MinPaths.relax(dg, sourceNode.getVertex(), endNode.getVertex() );
-                     endNode = endNode.getNextNode();
+                while (endNode != null) {
+                    MinPaths.relax(dg, sourceNode.getVertex(), endNode.getVertex());
+                    endNode = endNode.getNextNode();
                 }
-
-
             }
-
-
         }
+        //verificação de ciclo negativo
+
+        //iteração em cada lista de adjacencia
+        for (int i = 0; i < arrayOfAdjacentList.length; i++) {
+
+            //se a posição estiver vazia, próximo
+            if (arrayOfAdjacentList[i] == null) continue;
+
+            AdjacentList temp = arrayOfAdjacentList[i];
+            Node sourceNode = temp.getHeadNode();
+            Node endNode = sourceNode.getNextNode();
+
+            while (endNode != null) {
+
+                    if (endNode.getVertex().getDistance() > sourceNode.getVertex().getDistance() + dg.getWeightBetween(sourceNode, endNode)){
+                        return false;
+                    }
+            }
+        }
+        return true;
     }
+
 }
